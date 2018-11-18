7a1233df960886b9707fd467c2a3744c5a3ec74e
dima27661 <44540179+dima27661@users.noreply.github.com>
2018-11-18 21:19:35 +0200

    добавил градусник в комнате

--- IRrecvRele/IRrecvRele.ino
+++ IRrecvRele/IRrecvRele.ino
@@ -525,8 +525,66 @@ if ((millis() - sensor_sleep_time) > 30000) {
    sensor_sleep_time = millis();
 
     PrintOutTemperature();
+    PrintInTemperature();    
  }     
 }
+
+
+void PrintInTemperature()
+{
+  byte i;
+  byte present = 0;
+  byte type_s;
+  byte data[12];
+//  byte addr[8]={0x28, 0xFF, 0xEF, 0xAF, 0xB3, 0x16, 0x3, 0x32}; //градусник на улице
+  byte addr[8]={0x28, 0xFF, 0xE5, 0xA, 0x33, 0x17, 0x3, 0x1}; //градусник внутри 2  
+  
+  float celsius, fahrenheit;
+  
+  Serial.print("ROM =");
+  for( i = 0; i < 8; i++) {
+    Serial.write(' ');
+    Serial.print(addr[i], HEX);
+  }
+
+  if (OneWire::crc8(addr, 7) != addr[7]) {
+      Serial.println("CRC is not valid!");
+      return;
+  }
+ 
+  // the first ROM byte indicates which chip
+    type_s = 0;
+
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44, 1);        // start conversion, with parasite power on at the end
+  
+  delay(1000);     // maybe 750ms is enough, maybe not
+  
+  present = ds.reset();
+  ds.select(addr);    
+  ds.write(0xBE);         // Read Scratchpad
+
+  for ( i = 0; i < 9; i++) {           // we need 9 bytes
+    data[i] = ds.read();
+  }
+
+  int16_t raw = (data[1] << 8) | data[0];
+
+    byte cfg = (data[4] & 0x60);
+    // at lower res, the low bits are undefined, so let's zero them
+    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+    //// default is 12 bit resolution, 750 ms conversion time
+
+  celsius = (float)raw / 16.0;
+  fahrenheit = celsius * 1.8 + 32.0;
+     lcd.setCursor(10, 1); //set Starting position       
+     lcd.print(celsius);
+     lcd.print("C");     
+}
+
 
 void PrintOutTemperature()
 {
